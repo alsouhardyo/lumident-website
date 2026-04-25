364678d1e123f5f8bf4e562b7af3a01c5b5e6f38
Abdullah <abdullahalmahmud1113@gmail.com>
2026-04-25 17:50:34 +0600

    Lenis Scroll Modified

--- src/lib/utils/LenisScroll.tsx
+++ src/lib/utils/LenisScroll.tsx
@@ -7,7 +7,6 @@ const LenisProvider = ({ children }: { children: React.ReactNode }) => {
         const lenis = new Lenis({
             duration: 1.5,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            syncTouch: true,
             anchors: true
         });
         function raf(time: number) {
